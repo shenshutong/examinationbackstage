a652d51feb5a67aab47aca876e8876886fa5381d
shenshutong <13780892097@163.com>
2020-12-26 10:07:42 +0800

    Initial commit

---
+++ src/test/java/com/bgs/examinationbackstage/ExaminationbackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgs.examinationbackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationbackstageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
